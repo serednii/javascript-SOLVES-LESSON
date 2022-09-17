941520f39adf2567b4ea42952347e2b03f65f293
Serednii Mykola <seredniimykola@gmail.com>
2022-09-17 22:00:40 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,39 +1,28 @@
-$(document).ready(function () {
+//маємо progress bar який має заповнюватися за певний час, цей час задає програміст як параметр функції
+//We have a progress bar that must be filled in a certain time, this time is set by the programmer as a parameter of the function
 
-    showStartModal();
-});
+function userprogress(time) {
+    time = time * 10;
+    console.log(time)
+    let start = 0;
+    const progressElement = document.getElementById('user-progress');
 
+    setTimeout(function () {
+        let intervalID = setInterval(function () {
+            if (start < 100) {
+                progressElement.value = start;
+            } else {
+                clearInterval(intervalID);
+                userProgressCallBack();
+            }
+            start++;
+        }, time);
+    }, 2000);
 
-function showStartModal() {
-    //start timeout
-    if (localStorage.getItem('modalYesBtn') != 1) {
-        setTimeout(function () {
-            const startModal = `<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-primary" id="modal-yes-btn">Принимаю условия</button>
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    </div>
-                </div>
-            </div>
-        </div>`
-
-            let modal = document.querySelector('body');
-            modal.insertAdjacentHTML('afterbegin', startModal);
-
-            $('#exampleModal').modal('show');
-            $('#modal-yes-btn').on('click', function () {
-                //save state modal
-                localStorage.setItem('modalYesBtn', 1);
-                $('#exampleModal').modal('hide');
-            });
-        }, 1000);
+    function userProgressCallBack() {
+        //your-code
+        document.querySelector('.hidden-block').style.display = "block";
     }
 }
+
+userprogress(2);
